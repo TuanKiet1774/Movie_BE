78b05037bedf65964a65437c3a0c05f22fb84299
Phạm Tuấn Kiệt <167632480+TuanKiet1774@users.noreply.github.com>
2026-01-28 18:16:34 +0700

    Fix lỗi

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ const PORT = process.env.PORT || 3000;
 
 (async () => {
   try {
+    console.log("🚀 Starting server...");
     await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB connected");
 
-    app.listen(PORT, () => {
-      console.log(`🚀 Server running at http://localhost:${PORT}`);
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`🚀 Server running on port ${PORT}`);
     });
   } catch (error) {
     console.error("❌ Server startup failed:", error.message);
